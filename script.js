91e7b453868d79706c813801543f9297bfb94e9f
Tom Lister <rhxh35@durham.ac.uk>
2022-04-03 17:02:52 +0100

    Fixed Framework, Added Entries

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ let frame = 1;
 let finished = false;
 let selected = 1;
 let daily = {
-    "4-3":["The Unethicals","&#128184 "]
+    "4-3":["Anti-Sugar PSA","&#x1F36C ","1"],
+    "5-3":["Looped","&#x267B ","2"],
+    "6-3":["The MadD G Documentary","&#x1F4FD ","3"],
+    "7-3":["Family Guy Intro (HD)","&#x1F9CD ","4"],
+    "8-3":["Vaccines & Violence (An In-Depth Documentary)","&#x1F489 ","5"],
+    "9-3":["Poop Show","&#x1F4A9 ","6"],
+    "10-3":["The Final Accommodation","&#x1F3E0 ","7"],
+    "11-3":["How To Stop Diseases","&#x1F9EC ","8"],
+    "12-3":["Every Teen Movie Opening","&#x1F39E ","9"],
+    "13-3":["Is Chicken Even Real (An In-Depth Documentary)","&#x1F414 ","10"],
+    "14-3":["Anti-Sugar PSA (5th Anniversary Edition)","&#x1F36D ","11"]
 }
 let congrats = {
     1:"Gyro Genius (1/6)",
@@ -17,8 +27,15 @@ let day = d.getDate() + "-" + d.getMonth();
 
 let answer = daily[day][0];
 let emoji = daily[day][1];
+let number = daily[day][2];
 
 function check(e) {
+    let field = document.getElementById("film-guess");
+    if (field.value != "") {
+        field.setAttribute("list","film-titles")
+    } else {
+        field.setAttribute("list","")
+    }
     if(e.keyCode == 13 && !finished) {
         let field = document.getElementById("film-guess");
         let guess = field.value;
@@ -62,7 +79,7 @@ function check(e) {
         guesses.append(guessbox);
         guesses.append(newline);
 
-        selectFrame(frame)
+        if (frame < 7) {selectFrame(frame)};
     }
 };
 
@@ -82,14 +99,14 @@ function goodEnding(frame) {
     for (let i = frame+1; i < 7; i += 1) {
         let numberbox = document.createElement("div");
         numberbox.classList.add("number");
-        numberbox.id = i
-        numberbox.setAttribute("onclick","selectFrame(id)")
+        numberbox.id = i;
+        numberbox.setAttribute("onclick","selectFrame(id)");
         let numbertext = document.createElement("p");
         numbertext.innerHTML = String(i);
-        numberbox.append(numbertext)
+        numberbox.append(numbertext);
 
         let numbers = document.getElementById("numbers");
-        numbers.append(numberbox)
+        numbers.append(numberbox);
     }
     
     let entry = document.getElementById("entry");
@@ -107,17 +124,19 @@ function goodEnding(frame) {
     entry.append(congratsbox);
     entry.append(newline);
 
+    copyBox();
 };
 
 function badEnding() {
     let entry = document.getElementById("entry");
     let failurebox = document.createElement("div");
     failurebox.classList.add("guess");
+    failurebox.style.marginTop = "12px";
     let failuretext = document.createElement("p");
     let failurebold = document.createElement("strong");
     failurebold.innerHTML = answer;
     failuretext.append(failurebold)
-    failuretext.style.lineHeight = "normal";
+    failuretext.style.lineHeight = "90%";
     failuretext.style.color = "red";
     failurebox.append(failuretext);
     let newline = document.createElement("br");
@@ -125,6 +144,36 @@ function badEnding() {
     entry.innerHTML = "";
     entry.append(failurebox);
     entry.append(newline);
+
+    copyBox();
+}
+
+function copyBox() {
+    let copybox = document.createElement("div");
+    copybox.classList.add("number");
+    copybox.setAttribute("onclick","copyText()")
+    copybox.id = "copy-box";
+    let copytext = document.createElement("p");
+    copytext.id = "copy-text";
+    copytext.innerHTML = "Copy results";
+    copybox.append(copytext)
+    copybox.style.width = "90px";
+
+    let numbers = document.getElementById("numbers");
+    numbers.append(copybox)
+}
+
+function copyText() {
+    if (frame < 7) {
+        navigator.clipboard.writeText("Unethiverse Framed #"+number+"\n🎥 "+"🟥 ".repeat(frame-1)+"🟩"+" ⬛".repeat(6-frame)+"\nhttps://tom-lister.github.io/");
+    } else {
+        navigator.clipboard.writeText("Unethiverse Framed #"+number+"\n🎥"+"🟥 🟥 🟥 🟥 🟥 🟥\nhttps://tom-lister.github.io/");
+    };
+    document.getElementById("copy-text").innerHTML = "Copied!";
+    box = document.getElementById("copy-box");
+    box.setAttribute("onclick","");
+    box.style.cursor = "default";
+
 }
 
 document.getElementById("display").src = answer+"/1.png"
