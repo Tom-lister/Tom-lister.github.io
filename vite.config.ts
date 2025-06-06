64efdd7a250911f8106b6cb04cb50ed5992607cf
Tom <rhxh35@durham.ac.uk>
2025-06-06 19:36:30 +0100

    fix attempt

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/https://tom-lister.github.io/",
+  base: "/tom-lister.github.io/",
   plugins: [react()],
 })
